e422af5271b2c57af207ff05a608fa134ab7ef86
Deblina <deblinabarua2004@gmail.com>
2026-06-25 17:21:27 +0530

    Project

--- pages/emp_dash.py
+++ pages/emp_dash.py
@@ -26,9 +26,10 @@ tab1, tab2 = st.tabs(["Projects", "Add Tasks"])
 
 with tab1:
     view_projects = requests.post(f"{API_URL}/view_projects", json = {"empid": curr_user})
-    projects = view_projects.json()
+    
     
     if view_projects.status_code == 200: 
+        projects = view_projects.json()
         if not projects:   
             st.info("No projects.")
         else:
@@ -49,7 +50,7 @@ with tab2:
     curr_user = (st.session_state.user["empid"])
     get_projects = requests.post(f"{API_URL}/employee_projects", json = {"empid": curr_user}).json()
     with st.form("Add Task"):
-        project = st.selectbox("Project", get_projects, format_func = lambda x:x["title"])
+        project_name = st.selectbox("Project", get_projects, format_func = lambda x:x["title"])
         title = st.text_input("Task Title")
         description = st.text_area("Description")
         position = st.number_input("Position",min_value = 1, value = 1)
@@ -57,7 +58,7 @@ with tab2:
         
         submit = st.form_submit_button("Create Task")
         if submit:
-            add = requests.post(f"{API_URL}/create_task", json = {"projectid": project["projectid"], "title": title, "description": description, "position": position, "parent_task": int(parent) if parent else None, "created_by": curr_user, "members": [curr_user]})
+            add = requests.post(f"{API_URL}/create_task", json = {"projectid": project_name["projectid"], "title": title, "description": description, "position": position, "parent_task": int(parent) if parent else None, "created_by": curr_user, "members": [curr_user]})
             if add.status_code == 200:
                 st.success("Task added")
                 st.rerun()
