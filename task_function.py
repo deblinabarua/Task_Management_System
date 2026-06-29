96e9550580d41058c1de43a84347b175a505bd16
Deblina <deblinabarua2004@gmail.com>
2026-06-29 11:01:45 +0530

    Adding loading for sleeping Render and Neon.

--- task_function.py
+++ task_function.py
@@ -2,7 +2,14 @@ from task_database import SessionLocal, Employee
 from sqlalchemy import select
 import secrets
 import string
+import time
+import streamlit as st
+import requests
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+API_URL = os.getenv("API_URL")
 
 def generate_username(firstname, lastname):
     with SessionLocal() as db:
@@ -17,3 +24,17 @@ def generate_username(firstname, lastname):
 def generate_temp_pass():
     chars = string.ascii_letters + string.digits + "!@#$%"
     return "".join(secrets.choice(chars) for _ in range(10))
+
+def api_post(endpoint, payload, retries = 2):
+    for attempt in range(retries):
+        try:
+            with st.spinner("Starting server, please wait."):
+                response = requests.post(f"{API_URL}{endpoint}", json = payload, timeout = 30)
+                return response
+        except requests.exceptions.RequestException:
+            if attempt < retries - 1:
+                st.warning("Server is waking up. Please wait.")
+                time.sleep(5)
+
+        st.error("Server is taking longer than expected. Please try again later.")
+        return None
